4cd2ca9ce3de4e04b919001121b563e1b1f1ffc9
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2024-09-02 20:03:20 +0700

    Qt: Ensure window is raised after recreation
    Backport from: https://github.com/stenzek/duckstation/commit/54c79ff937d7b4d455d54296ee35d7cbd906a916

--- pcsx2-qt/MainWindow.cpp
+++ pcsx2-qt/MainWindow.cpp
@@ -2337,6 +2337,7 @@ std::optional<WindowInfo> MainWindow::acquireRenderWindow(bool recreate_window, 
 	updateWindowState();
 
 	updateDisplayWidgetCursor();
+	QtUtils::ShowOrRaiseWindow(QtUtils::GetRootWidget(m_display_widget));
 	m_display_widget->setFocus();
 
 	return wi;
@@ -2409,6 +2410,7 @@ void MainWindow::createDisplayWidget(bool fullscreen, bool render_to_main)
 		m_ui.mainContainer->addWidget(container);
 		m_ui.mainContainer->setCurrentIndex(1);
 	}
+
 
 	updateDisplayRelatedActions(true, render_to_main, fullscreen);
 
