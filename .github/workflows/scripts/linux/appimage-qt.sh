b4e97a8fee5c4694165aa17783252182f8c6783b
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2024-09-23 16:27:33 +0700

    Deps: Fix Builds

--- .github/workflows/scripts/linux/appimage-qt.sh
+++ .github/workflows/scripts/linux/appimage-qt.sh
@@ -42,7 +42,7 @@ APPDIRNAME=PCSX2.AppDir
 STRIP=strip
 
 declare -a MANUAL_LIBS=(
-	"libfreetype.so"
+	"libfreetype.so.6"
 	"libshaderc_shared.so"
 )
 
@@ -91,21 +91,16 @@ OUTDIR=$(realpath "./$APPDIRNAME")
 rm -fr "$OUTDIR"
 
 echo "Locating extra libraries..."
-EXTRA_LIBS_ARGS=""
+EXTRA_LIBS_ARGS=()
 for lib in "${MANUAL_LIBS[@]}"; do
 	srcpath=$(find "$DEPSDIR" -name "$lib")
 	if [ ! -f "$srcpath" ]; then
-		echo "Missinge extra library $lib. Exiting."
+		echo "Missing extra library $lib. Exiting."
 		exit 1
 	fi
 
 	echo "Found $lib at $srcpath."
-
-	if [ "$EXTRA_LIBS_ARGS" == "" ]; then
-		EXTRA_LIBS_ARGS="--library=$srcpath"
-	else
-		EXTRA_LIBS_ARGS="$EXTRA_LIBS_ARGS,$srcpath"
-	fi
+	EXTRA_LIBS_ARGS+=("--library=$srcpath")
 done
 
 # Why the nastyness? linuxdeploy strips our main binary, and there's no option to turn it off.
