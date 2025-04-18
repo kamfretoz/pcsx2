463db63cc5b2300ca493d351a9e6964405f7a0a4
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-04-18 16:40:21 +0700

    GS/HW: Add ROV based rendering for DX11/DX12/Vulkan

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -832,6 +832,7 @@ bool Pcsx2Config::GSOptions::OptionsAreEqual(const GSOptions& right) const
 		OpEqu(UserHacks_TextureInsideRt) &&
 		OpEqu(UserHacks_BilinearHack) &&
 		OpEqu(OverrideTextureBarriers) &&
+		OpEqu(OverrideRasterizerOrderViews) &&
 
 		OpEqu(CAS_Sharpness) &&
 		OpEqu(ShadeBoost_Brightness) &&
@@ -882,6 +883,7 @@ bool Pcsx2Config::GSOptions::RestartOptionsAreEqual(const GSOptions& right) cons
 		   OpEqu(DisableFramebufferFetch) &&
 		   OpEqu(DisableVertexShaderExpand) &&
 		   OpEqu(OverrideTextureBarriers) &&
+		   OpEqu(OverrideRasterizerOrderViews) &&
 		   OpEqu(ExclusiveFullscreenControl);
 }
 
@@ -1024,6 +1026,7 @@ void Pcsx2Config::GSOptions::LoadSave(SettingsWrapper& wrap)
 	SettingsWrapIntEnumEx(UserHacks_GPUTargetCLUTMode, "UserHacks_GPUTargetCLUTMode");
 	SettingsWrapIntEnumEx(TriFilter, "TriFilter");
 	SettingsWrapBitfieldEx(OverrideTextureBarriers, "OverrideTextureBarriers");
+	SettingsWrapBitfieldEx(OverrideRasterizerOrderViews, "OverrideRasterizerOrderViews");
 
 	SettingsWrapBitfield(ShadeBoost_Brightness);
 	SettingsWrapBitfield(ShadeBoost_Contrast);
